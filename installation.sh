457f5267072f5ecdc8b370105a569d30b84575bb
kalpana <128675891+kalpanaIronbanda@users.noreply.github.com>
2023-05-30 15:56:00 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -5,7 +5,7 @@ cd /opt
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-9.5.2.linux-amd64.tar.gz
 tar -zxvf grafana-enterprise-9.5.2.linux-amd64.tar.gz
 rm -rf grafana-enterprise-9.5.2.linux-amd64.tar.gz
-ln -s /opt/grafana-enterprise-9.5.2 grafana
+ln -s /opt/grafana-9.5.2 grafana
 chmod -R 755 grafana
 cd grafana/bin/
 nohup ./grafana-server & 
